fa595884d9151049f1af25f45f285111775d5058
Jihun Choi <tkarnrwlehtk@gmail.com>
2022-10-05 15:41:37 -0700

    code updated
    code updated

---
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.myquizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "What is 1 + 1?"
+
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"0","1","2","3"}
+
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Notepad",
+            "Youtube",
+            "2"
+    };
+};
